0fd061f301841e390b27638ce4e8b09e3ba46be4
Byron Torres <byron.torres@telus.com>
2019-07-10 23:26:27 -0500

    AP-001: Byron/Keto/Eduardo/Kevin/Mauro/Chichan - Crear gets para servicio de frases y agregar base de datos

--- readme.txt
+++ readme.txt
@@ -1,1 +1,15 @@
-Codigo Maldito
+Requisitos
+Instalar Git
+
+Instalar Node
+https://nodejs.org/en/download/
+
+Instalar Dependencias
+    Express
+    npm install express --save
+
+    Sqlite
+    npm install sqlite3 --save
+
+Para ejecutar el proyecto
+Node server.js
--- server.js
+++ server.js
@@ -1,13 +1,37 @@
-var express = require('express');
+var express = require("express");
+var sqlite3 = require("sqlite3");
+var database = new sqlite3.Database("frases.db");
 
 var app = express();
 
 var port = 5000;
 
-app.listen(port, function(){
-    console.log("Express app listening on port " + port);
+app.listen(port, function() {
+  console.log("Express app listening on port " + port);
 });
 
-app.get('/', function(request, response){
-    response.send('Hello, World!');
+app.get("/", function(request, response) {
+  response.send("Hello, World!");
+});
+
+app.get("/frases", function(request, response) {
+  database.all("SELECT * FROM Frases", function(err, rows) {
+    console.log(
+      "GET Frases: La base de datos tiene las siguientes frases" + rows
+    );
+
+    response.send(rows);
+  });
+});
+
+app.get("/frases/:autor", function(request, response) {
+  database.all(
+    "SELECT * FROM Frases WHERE Autor= ?",
+    [request.params.autor],
+    function(err, rows) {
+      console.log("GET Frases por autor:" + request.params.autor);
+
+      response.send(rows);
+    }
+  );
 });
